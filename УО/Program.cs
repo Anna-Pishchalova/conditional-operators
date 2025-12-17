e80ac1182def98696ae2bf7b7950174e5bda3479
pr-24.106-pa <pr-24.106-pa@115-01>
2025-12-17 14:29:22 +0700

    Добавьте файлы проекта.

---
+++ УО/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace УО
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = 10;
+            switch (t)
+            {
+                case 1 when (t < 0):
+                    {
+                        Console.WriteLine("Зима");
+                    }
+                break;
+                case 2 when (t >= 0 && t <= 15):
+                    {
+                        Console.WriteLine("Весна/осень");
+                    }
+                break;
+                case 3 when (t >= 15):
+                    {
+                        Console.WriteLine("Лето");
+                    }
+                break;
+            }
+        }
+    }
+}
